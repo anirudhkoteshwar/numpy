5b39be2a1e989f1e6b8459f8d18e263656ecacbc
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-20 12:29:25 +0530

    added accesselements.py

--- accesselements.py
+++ accesselements.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+a = np.array([[1,2,3,4,5,6,7,8,9,10],[10,20,30,40,50,60,70,80,90,100]])
+#print(a.shape) #confirm dimentions of array
+
+# get specific element using [r,c]
+print(a[0,5]) #6
+
+# get a row
+print(a[0,:])
